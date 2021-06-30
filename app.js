566cb6e218a7dc4086bc7be98f4c06787b71261c
Sangpil Jung <59302973+Thinkteam20@users.noreply.github.com>
2021-06-30 17:37:29 +1000

    toggle

--- app.js
+++ app.js
@@ -1,12 +1,7 @@
 const h1 = document.querySelector("h1");
 
 function handleMouseEnter() {
-  const clickedClass = "clicked";
-  if (h1.classList.contains(clickedClass)) {
-    h1.classList.remove(clickedClass);
-  } else {
-    h1.classList.add(clickedClass);
-  }
+  h1.classList.toggle("clicked");
 }
 
 h1.addEventListener("click", handleMouseEnter);
