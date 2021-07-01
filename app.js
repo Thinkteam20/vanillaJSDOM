eb2788b4bf5a058446623e954d141cff36f02211
Sangpil Jung <sangpiljung@Sangpils-MBP.modem>
2021-07-01 19:57:56 +1000

    DOM event prac

--- app.js
+++ app.js
@@ -1,7 +1,31 @@
-const h1 = document.querySelector("h1");
+colors =["#1abc9c", "#3498db", "#9b59b6", "#f39c12", "#e74c3c"]
 
-function handleMouseEnter() {
-  h1.classList.toggle("clicked");
+const greeting = document.querySelector("h1");
+
+//console.log(greeting);
+
+greeting.style.color = colors[4];
+
+const handlers = {
+  mouseEnter: function(){
+    greeting.innerHTML = "The mouse is here"
+    greeting.style.color = colors[0];
+  },
+  mouseLeft: function(){
+    greeting.innerHTML ="The mouse is left";
+    greeting.style.color = colors[1];
+  },
+  windowResized: function(){
+    greeting.innerHTML = "The window is resized"
+    greeting.style.color = colors[2];
+  },
+  mouseRightClicked: function(){
+    greeting.innerHTML = "The mouse is right-clicked";
+    greeting.style.color = colors[3];
+  }
 }
 
-h1.addEventListener("click", handleMouseEnter);
+greeting.addEventListener("mouseenter",handlers.mouseEnter);
+greeting.addEventListener("mouseleave",handlers.mouseLeft);
+window.addEventListener("resize",handlers.windowResized);
+document.addEventListener("contextmenu",handlers.mouseRightClicked);
