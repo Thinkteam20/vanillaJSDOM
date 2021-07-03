81aeb3e224dfd962a67ca9b71ef650f53a178bda
Sangpil Jung <sangpiljung@Sangpils-MBP.modem>
2021-07-03 23:38:47 +1000

    Input value check

--- app.js
+++ app.js
@@ -1,31 +1,9 @@
-colors =["#1abc9c", "#3498db", "#9b59b6", "#f39c12", "#e74c3c"]
+const loginInput = document.querySelector(".login-form input");
+const loginButton = document.querySelector(".login-form button");
 
-const greeting = document.querySelector("h1");
-
-//console.log(greeting);
-
-greeting.style.color = colors[4];
-
-const handlers = {
-  mouseEnter: function(){
-    greeting.innerHTML = "The mouse is here"
-    greeting.style.color = colors[0];
-  },
-  mouseLeft: function(){
-    greeting.innerHTML ="The mouse is left";
-    greeting.style.color = colors[1];
-  },
-  windowResized: function(){
-    greeting.innerHTML = "The window is resized"
-    greeting.style.color = colors[2];
-  },
-  mouseRightClicked: function(){
-    greeting.innerHTML = "The mouse is right-clicked";
-    greeting.style.color = colors[3];
-  }
+function handleLoginButton() {
+  //console.dir(loginInput);
+  console.log(loginInput.value);
 }
 
-greeting.addEventListener("mouseenter",handlers.mouseEnter);
-greeting.addEventListener("mouseleave",handlers.mouseLeft);
-window.addEventListener("resize",handlers.windowResized);
-document.addEventListener("contextmenu",handlers.mouseRightClicked);
+loginButton.addEventListener("click", handleLoginButton);
