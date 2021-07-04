b612e7587364304dd095ada38a2f331d3c0deb61
Sangpil Jung <sangpiljung@Sangpils-MBP.modem>
2021-07-05 09:43:07 +1000

    link event

--- index.js
+++ index.js
@@ -2,10 +2,19 @@ const loginForm = document.querySelector(".login-form");
 const loginInput = document.querySelector(".login-form input");
 const loginButton = document.querySelector(".login-form button");
 
+const link = document.querySelector("a");
+
 function onLoginSubmit(e) {
   e.preventDefault();
   const username = loginInput.value;
   console.log(username);
 }
 
+function handleLink(e) {
+  //alert("Click.");
+  e.preventDefault();
+  console.log(e);
+}
+
 loginForm.addEventListener("submit", onLoginSubmit);
+link.addEventListener("click", handleLink);
