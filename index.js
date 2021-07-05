dd8f2e48d477116a2510eb479c2ffcad74dd0db4
Sangpil Jung <sangpiljung@Sangpils-MBP.modem>
2021-07-05 10:39:31 +1000

    improve code , use of localStorage on broweser)

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
-const loginForm = document.querySelector(".login-form");
-const loginInput = document.querySelector(".login-form input");
-const loginButton = document.querySelector(".login-form button");
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const loginButton = document.querySelector("#login-form button");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
 // convention uppercase variable is used when i want to save STRING
+const USERNAME_KEY = "username";
+// it is also can be used when STRING is used in many place
 
 function onLoginSubmit(e) {
   e.preventDefault();
-  const username = loginInput.value;
   loginForm.classList.add(HIDDEN_CLASSNAME);
+  const username = loginInput.value;
+  localStorage.setItem(USERNAME_KEY, username);
   //greeting.innerHTML = "Hello " + username;
-  greeting.innerHTML = `Hello ${username}`;
-  greeting.classList.remove(HIDDEN_CLASSNAME);
+  paintGreetings(username);
 }
 
 function handleLink(e) {
@@ -21,4 +23,20 @@ function handleLink(e) {
   console.log(e);
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
+function paintGreetings(username) {
+  greeting.innerText = `Hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+const savedUserName = localStorage.getItem(USERNAME_KEY);
+
+//console.log(savedUserName);
+
+if (savedUserName === null) {
+  //show the form
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+  //show the username
+  paintGreetings(savedUserName);
+}
